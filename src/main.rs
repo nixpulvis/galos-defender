575ebc8bfd0191b681927bf4fba069a74b0eb302
Nathan Lilienthal <nathan@nixpulvis.com>
2024-10-02 17:48:45 -0400

    Update to use position

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
     let mut app = App::new()
         // .add_plugins(DefaultPlugins)
         .add_systems(Startup, spawn)
-        .add_systems(Update, query)
+        .add_systems(Update, query.after(expand))
         .add_systems(Update, check_expansion)
-        .add_systems(Update, expand)
+        .add_systems(Update, expand.after(check_expansion))
         .add_event::<Expand>()
         .run();
 }
@@ -32,6 +32,9 @@ struct Factions(Vec<Entity>);
 #[derive(Component, Debug)]
 struct Address(u32);
 
+#[derive(Component, Debug)]
+struct Position(Vec3);
+
 #[derive(Bundle, Debug)]
 struct FactionBundle {
     faction: Faction,
@@ -43,6 +46,7 @@ struct SystemBundle {
     system: System,
     address: Address,
     name: Name,
+    position: Position,
 }
 
 #[derive(Event)]
@@ -58,50 +62,63 @@ fn spawn(mut commands: Commands) {
             name: Name("Our Faction".into()),
         },))
         .id();
+
     let system_a = commands
         .spawn((
             SystemBundle {
                 system: System,
                 address: Address(0),
                 name: Name("SOL".into()),
+                position: Position(Vec3::splat(0.)),
             },
             Factions(vec![faction_a]),
         ))
         .id();
 
-    let system_b = commands
-        .spawn((
-            SystemBundle {
-                system: System,
-                address: Address(1),
-                name: Name("Neighbor 1".into()),
-            },
-            Neighbors(vec![system_a]),
-        ))
+    let neighbor = commands
+        .spawn((SystemBundle {
+            system: System,
+            address: Address(1),
+            name: Name("ALPHA CENTAURI".into()),
+            position: Position(Vec3::new(3.03125, -0.09375, 3.15625)),
+        },))
         .id();
 
-    commands.get_entity(system_a).map(|mut cmds| {
-        cmds.insert(Neighbors(vec![system_b]));
-    });
+    let not_neighbor = commands
+        .spawn((SystemBundle {
+            system: System,
+            address: Address(2),
+            name: Name("G 139-21".into()),
+            position: Position(Vec3::new(-17.03125, 16.875, 34.625)),
+        },))
+        .id();
+
     commands.get_entity(faction_a).map(|mut cmds| {
         cmds.insert(Systems(vec![system_a]));
     });
 }
 
-fn query(systems: Query<(&Name, &Factions), With<System>>, factions: Query<&Name, With<Faction>>) {
+fn query(
+    systems: Query<(&Name, Option<&Factions>), With<System>>,
+    factions: Query<&Name, With<Faction>>,
+) {
     for (system_name, system_factions) in &systems {
         dbg!(system_name);
-        for faction in &system_factions.0 {
-            let Ok(faction_name) = factions.get(*faction) else {
-                continue;
-            };
-            dbg!(faction_name);
+        if let Some(sf) = system_factions {
+            for faction in &sf.0 {
+                let Ok(faction_name) = factions.get(*faction) else {
+                    continue;
+                };
+                dbg!(faction_name);
+            }
         }
     }
 }
 
 fn expand(mut ev_r: EventReader<Expand>, mut target_system_query: Query<&mut Factions>) {
     for event in ev_r.read() {
+        println!("Processing Expand event");
+        // TODO: Handle creating a new Factions when it doesn't exist.
         let Ok(mut faction_list) = target_system_query.get_mut(event.system) else {
             continue;
         };
@@ -111,17 +128,20 @@ fn expand(mut ev_r: EventReader<Expand>, mut target_system_query: Query<&mut Fac
 
 fn check_expansion(
     mut ev_w: EventWriter<Expand>,
-    system_query: Query<(&Factions, &Neighbors), With<System>>,
+    system_query: Query<(Entity, &Name, &Position, &Factions), With<System>>,
 ) {
-    for (factions, neighbors) in &system_query {
-        // do some additional queries on the entity lists to determine which factions should
-        // expand, and where. in this example, all factions expand to all neighbors
-        for neighbor in neighbors.0.clone() {
-            for faction in factions.0.clone() {
-                ev_w.send(Expand {
-                    faction,
-                    system: neighbor,
-                });
+    // do some additional queries on the entity lists to determine which factions should
+    // expand, and where. in this example, all factions expand to all neighbors
+    for (system_a, name_a, position_a, factions_a) in &system_query {
+        for (system_b, name_b, position_b, factions_b) in &system_query {
+            if position_a.0.distance(position_b.0) < 20. {
+                for faction in factions_a.0.clone() {
+                    println!("Sending Expand event");
+                    ev_w.send(Expand {
+                        faction,
+                        system: system_b,
+                    });
+                }
             }
         }
     }
